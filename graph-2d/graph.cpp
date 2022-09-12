b4ae6f781e6a0d447455612787ce2e018ba1fede
Joey Yang <joeyyang.ai@gmail.com>
2022-09-12 19:57:13 -0400

    Implement addEdge(), addBidirectionalEdge(), and printGraph(); remove unnecessary Edge struct

--- graph-2d/graph.cpp
+++ graph-2d/graph.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+#include <stdexcept>
 #include <unordered_map>
 #include <vector>
 
@@ -8,19 +10,7 @@ template <typename T>
 using EdgeWeight = T;
 
 template <typename TId, typename TWeight>
-struct Edge
-{
-    Edge(const VertexId<TId> dest_id, const EdgeWeight<TWeight> weight) : dest_id_(dest_id), weight_(weight)
-    {
-    }
-
-    VertexId<TId> dest_id_;
-    EdgeWeight<TWeight> weight_;
-};
-
-template <typename TId, typename TWeight>
-using EdgeList = std::vector<Edge<TId, TWeight>>;
-//using EdgeList = std::unordered_map<VertexId, EdgeWeight>;
+using EdgeList = std::unordered_map<VertexId<TId>, EdgeWeight<TWeight>>;
 
 template <typename TId, typename TWeight>
 using AdjacencyList = std::unordered_map<VertexId<TId>, EdgeList<TId, TWeight>>;
@@ -37,20 +27,60 @@ public:
     {
     }
 
+    void addEdge(const VertexId<TId> src_id, const VertexId<TId> dest_id, const EdgeWeight<TWeight> weight)
+    {
+        // Source vertex with edge(s) already exists in graph.
+        if (graph_.contains(src_id))
+        {
+            // Destination vertex from source vertex already exists.
+            if (graph_[src_id].contains(dest_id))
+            {
+                throw std::invalid_argument("Could not add edge to vertex. Edge already exists!");
+            }
+        }
+
+        graph_[src_id].emplace(dest_id, weight);
+    }
+
+    void addBidirectionalEdge(const VertexId<TId> src_id, const VertexId<TId> dest_id, const EdgeWeight<TWeight> weight)
+    {
+        addEdge(src_id, dest_id, weight);
+        addEdge(dest_id, src_id, weight);
+    }
+
     // Unnecessary to expose in API.
     //const AdjacencyList& getGraph()
     //{
     //    return graph_;
     //}
 
+    template <typename TIdFriend, typename TWeightFriend>
+    friend void printGraph(const Graph<TIdFriend, TWeightFriend>& graph);
+
 private:
     AdjacencyList<TId, TWeight> graph_;
 };
 
-int main(int argc, char* argv[])
+template <typename TId, typename TWeight>
+void printGraph(const Graph<TId, TWeight>& graph)
+{
+    for (const auto& [src_id, edge_list] : graph.graph_)
+    {
+        std::cout << src_id << " : ";
+
+        for (const auto& [dest_id, weight] : edge_list)
+        {
+            std::cout << "(" << dest_id << ", " << weight << ") ";
+        }
+        
+        std::cout << "\n";
+    }
+}
+
+/*int main(int argc, char* argv[])
 {
     // Non-negative weights.
     Graph<char, unsigned int> graph;
 
     return 0;
-}
+}*/
