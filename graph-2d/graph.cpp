f8febc04997b7e2b1bf34a9a457bec80beb90e0f
Joey Yang <joeyyang.ai@gmail.com>
2022-09-12 10:23:01 -0400

    Remove simple graph; add templates to graph for generic types

--- graph-2d/graph.cpp
+++ graph-2d/graph.cpp
@@ -1,29 +1,35 @@
 #include <unordered_map>
 #include <vector>
 
-using VertexId = int;
+template <typename T>
+using VertexId = T;
 
-// Non-negative weights.
-using EdgeWeight = unsigned int;
+template <typename T>
+using EdgeWeight = T;
 
+template <typename TId, typename TWeight>
 struct Edge
 {
-    Edge(const VertexId dest_id, const EdgeWeight weight) : dest_id_(dest_id), weight_(weight)
+    Edge(const VertexId<TId> dest_id, const EdgeWeight<TWeight> weight) : dest_id_(dest_id), weight_(weight)
     {
     }
 
-    VertexId dest_id_;
-    EdgeWeight weight_;
+    VertexId<TId> dest_id_;
+    EdgeWeight<TWeight> weight_;
 };
 
-using EdgeList = std::vector<Edge>;
-// using EdgeList = std::unordered_map<VertexId, EdgeWeight>;
+template <typename TId, typename TWeight>
+using EdgeList = std::vector<Edge<TId, TWeight>>;
+//using EdgeList = std::unordered_map<VertexId, EdgeWeight>;
 
-using AdjacencyList = std::unordered_map<VertexId, EdgeList>;
+template <typename TId, typename TWeight>
+using AdjacencyList = std::unordered_map<VertexId<TId>, EdgeList<TId, TWeight>>;
 
 // This is minimalistically correct.
-//using Graph = AdjacencyList;
+//template <typename TId, typename TWeight>
+//using Graph = AdjacencyList<TId, TWeight>;
 
+template <typename TId, typename TWeight>
 class Graph
 {
 public:
@@ -32,18 +38,19 @@ public:
     }
 
     // Unnecessary to expose in API.
-    /*const AdjacencyList& getGraph()
-    {
-        return graph_;
-    }*/
+    //const AdjacencyList& getGraph()
+    //{
+    //    return graph_;
+    //}
 
 private:
-    AdjacencyList graph_;
+    AdjacencyList<TId, TWeight> graph_;
 };
 
 int main(int argc, char* argv[])
 {
-    Graph graph;
+    // Non-negative weights.
+    Graph<char, unsigned int> graph;
 
     return 0;
 }
